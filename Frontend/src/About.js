5f91452f8515512254cad91d2a4b0be1abe18e00
Aaron Light <aaronl@tx.rr.com>
2023-02-05 15:44:12 -0700

    added photos to about section

--- Frontend/src/About.js
+++ Frontend/src/About.js
@@ -1,5 +1,6 @@
 import DisplayParagraphAndImage from "./Paragraph and image section/DisplayParagraphAndImage"
-import italy_hike from "./Assets/italy_hike.jpg"
+import utah from "./Assets/utah.jpg"
+import portrait from "./Assets/portrait.jpg"
 import canyon_2 from "./Assets/canyon_2.jpg"
 import TopBanner from "./TopBanner";
 
@@ -10,12 +11,12 @@ const About = () => {
             id: 1, 
             title: "About This Website", 
             pgraph: "Taking the form of a professional tour guide company's website, this project is a digital journal of vacations, day trips, or adventurous activities that I have done in the past. Every trip, sample itinerary, and photo is from my own past experiences. Hopefully this journal will inspire future adventures for its users!", 
-            image: italy_hike
+            image: utah
         },
         {
             id: 2, title: "About The Author", 
-            pgraph: "I a am self-taught software engineer with a background in Aerospace Engineering from The University of Texas at Austin. I have been a fulltime engineer at Northrop Grumman since 8/2021. My skills include React, Ruby on Rails, Python, Java, C, C#, SQL, HTML/CSS, Matlab, and Object-Oriented Programming.", 
-            image: italy_hike
+            pgraph: "I am a self-taught software engineer with a background in Aerospace Engineering from The University of Texas at Austin. I have been a fulltime engineer at Northrop Grumman since 8/2021. My skills include React, Ruby on Rails, Python, Java, C, C#, SQL, HTML/CSS, Matlab, and Object-Oriented Programming.", 
+            image: portrait
         }
     ]
 
