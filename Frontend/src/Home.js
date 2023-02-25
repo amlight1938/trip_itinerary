fa7edf9b372b6e70c6d9b007dadc4561d9bebcc2
Aaron Light <aaronl@tx.rr.com>
2023-02-24 23:12:25 -0700

    finished trip itineraries seed file and added volcano hike to featured on home page

--- Frontend/src/Home.js
+++ Frontend/src/Home.js
@@ -11,14 +11,14 @@ import TripCard from "./Destinations/TripCard";
 import mountain_view from './Assets/mountain_view.jpg'
 import Alaska_sign from './Assets/Alaska_sign.jpg'
 import rappel_1 from './Assets/rappel_1.jpg'
-import peak from './Assets/peak.jpg'
+import volcano from './Assets/volcano.jpg'
 
 const Home = (props) => {
     const featured_trips = ([
-        {id: 8, image: mountain_view, name: 'Pacific Northwest Roadtrip', description: 'Explore Olympic National Park\'s backcountry, Seattle, and Ruby Beach. Enjoy breathtaking views as you go skydiving over Olympic National Park'},
+        {id: 1, image: mountain_view, name: 'Pacific Northwest Roadtrip', description: 'Explore Olympic National Park\'s backcountry, Seattle, and Ruby Beach. Enjoy breathtaking views as you go skydiving over Olympic National Park'},
         {id: 2, image: Alaska_sign, name: 'Bike from Texas to Alaska', description: 'Ride a bicycle 4,000 miles over 70 days from Austin, TX to Anchorage, AK. See North America from a new perspective and spend nights in host homes or camping in the wilderness'},
-        {id: 6, image: rappel_1, name: 'Rock Climbing Overnight', description: 'Camp in remote territory and conquer your fear of heights while climbing towering cliffs for two days'},
-        {id: 7, image: peak, name: 'Climb a 14er', description: 'Camp at the base of Mt. Democrat and hike 2,000ft to the summit in the early morning'},
+        {id: 3, image: rappel_1, name: 'Rock Climbing Overnight', description: 'Camp in remote territory and conquer your fear of heights while climbing towering cliffs for two days'},
+        {id: 4, image: volcano, name: 'Guatemala Volcano Hike', description: 'Climb an active volcano, explore Antigua and Panajachel, and experience Lake Atitlan from thousands of feet up while paragliding'},
     ])
 
     return (
